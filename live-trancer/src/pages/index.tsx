cdf2ed6f090b78d096a15dc2f80ec0eb3d6bd23e
YoungmanCH <tsubasa22business@gmail.com>
2024-10-15 21:18:33 +0900

    Added: EmailJS

--- live-trancer/src/pages/index.tsx
+++ live-trancer/src/pages/index.tsx
@@ -1,33 +1,15 @@
-// import { GetServerSideProps } from "next";
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <p>Redirecting to Home...</p>
-//     </div>
-//   );
-// }
-
-// export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-//   res.writeHead(302, { Location: '/home' });
-//   res.end();
-//   return { props: {} };
-// };
-
-
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/home");
-  }, [router]);
-
   return (
     <div>
       <p>Redirecting to Home...</p>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.writeHead(302, { Location: "/home" });
+  res.end();
+  return { props: {} };
+};
