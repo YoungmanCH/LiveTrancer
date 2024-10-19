da2960607548d78ed5dbb8d5a08d4f6eaf70384d
YoungmanCH <tsubasa22business@gmail.com>
2024-10-19 23:03:30 +0900

    Modified: /microservices/stt, utils, api

--- microservices/api/main.py
+++ microservices/api/main.py
@@ -2,117 +2,32 @@ import numpy as np
 from flask import Flask
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-from stt.src import stt
-# from integration.transcription.src import stt_to_tranc
-# from integration.transcription.test import test_stt_to_wav
-# from integration.transcription.test import test_stt_to_seconds10
-# from integration.transcription.test import test_stt
+from LiveTrancer.microservices.stt.src import stt
+from LiveTrancer.microservices.utils import save_audio
 
 app = Flask(__name__)
 CORS(app)  # CORSを全オリジンに対して許可
 socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
 
-### <- stt_to_tranc.py。今は、stt.pyでも併用してます ->
 # WebSocket接続時の処理
 @socketio.on('connect')
 def handle_connect():
     print("クライアントが接続されました")
     emit('response', {'message': 'WebSocketに接続しました'})
 
-### <- stt.py ->
-# WebSocketでSTTリクエストを受信
 @socketio.on('stt')
 def handle_stt(audio_data):
     try:
-        # audio_dataを受信し、test_stt_to_wavモジュールの関数に処理を委譲
         print("音声データを受信しました")
         data = np.frombuffer(audio_data, dtype=np.int16)
 
-        # 音声データをtest_stt_to_wavに渡して処理
-        stt.process_audio(data)
+        save_audio.save_audio(data)
+        audio_processor = stt.STTAudioProcessor.get_instance()
+        audio_processor.process_audio(data)
 
     except Exception as e:
         emit('stt_response', {'error': str(e)})
 
-
-### <- stt_to_tranc.py ->
-# # WebSocketでSTTリクエストを受信
-# @socketio.on('stt')
-# def handle_stt(audio_data):
-#     try:
-#         # audio_dataを受信し、test_stt_to_wavモジュールの関数に処理を委譲
-#         print("音声データを受信しました")
-#         data = np.frombuffer(audio_data, dtype=np.int16)
-
-#         # 音声データをtest_stt_to_wavに渡して処理
-#         stt_to_tranc.process_audio(data)
-
-#     except Exception as e:
-#         emit('stt_response', {'error': str(e)})
-
-
-### <- test_stt.py ->
-# # WebSocketでSTTリクエストを受信
-# @socketio.on('stt')
-# def handle_stt(audio_data):
-#     try:
-#         # audio_dataを受信し、test_stt_to_wavモジュールの関数に処理を委譲
-#         print("音声データを受信しました")
-#         data = np.frombuffer(audio_data, dtype=np.int16)
-
-#         # 音声データをtest_stt_to_wavに渡して処理
-#         test_stt.process_audio(data)
-
-#     except Exception as e:
-#         emit('stt_response', {'error': str(e)})
-
-
-### <- test_stt_to_seconds10.py ->
-# # WebSocketでSTTリクエストを受信
-# @socketio.on('stt')
-# def handle_stt(audio_data):
-#     try:
-#         # audio_dataを受信し、test_stt_to_wavモジュールの関数に処理を委譲
-#         print("音声データを受信しました")
-#         data = np.frombuffer(audio_data, dtype=np.int16)
-
-#         # 音声データをtest_stt_to_wavに渡して処理
-#         test_stt_to_seconds10.process_audio(data)
-
-#     except Exception as e:
-#         emit('stt_response', {'error': str(e)})
-
-
-### <- test_stt_to_wav.py ->
-# @socketio.on('stt')
-# def handle_stt(audio_data):
-#     try:
-#         # audio_dataを受信し、test_stt_to_wavモジュールの関数に処理を委譲
-#         print("音声データを受信しました")
-#         data = np.frombuffer(audio_data, dtype=np.int16)
-
-#         # 音声データをtest_stt_to_wavに渡して処理
-#         test_stt_to_wav.process_audio(data)
-
-#     except Exception as e:
-#         emit('stt_response', {'error': str(e)})
-
-
-### <- stt_to_tranc.py ->
-# @socketio.on('stt')
-# def handle_stt(audio_data):
-#     try:
-#         # audio_dataを受信し、STT処理を行う
-#         print("音声データを受信しました")
-#         data = np.frombuffer(audio_data, dtype=np.int16)
-#         print(f"Audio data type: {data.dtype}")
-#         text = stt_to_tranc.main(data)  # `main()`をaudio_dataを受け取る形式に変更
-#         emit('stt_response', {'text': text})
-#         print(f'解析したテキスト {text}')
-#     except Exception as e:
-#         emit('stt_response', {'error': str(e)})
-
-# WebSocketでTTSリクエストを受信
 @socketio.on('tts')
 def handle_tts(data):
     try:
---
+++ microservices/utils/save_audio.py
@@ -0,0 +1,28 @@
+import wave
+import numpy as np
+import time
+from LiveTrancer.microservices.utils import audio_file_saver
+
+audio_frames = []
+start_time = None
+auto_save_duration = 10 # 一定秒数録音後後に自動保存する閾値（秒）
+
+def save_audio(data):
+    """
+    音声データを蓄積し、一定時間の音声データが収集されたらWAVファイルに保存する。
+    """
+    global start_time, audio_frames
+
+    audio_frames.append(data)
+
+    if start_time is None:
+        start_time = time.time()
+
+    if time.time() - start_time >= auto_save_duration:
+        combined_data = np.concatenate(audio_frames).tobytes()
+        audioFileSaver = audio_file_saver.AudioFileSaver()
+        audioFileSaver.save_original_audio(combined_data)
+
+        # 音声データと開始時間をリセット
+        audio_frames = []
+        start_time = None
