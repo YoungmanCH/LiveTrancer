5e11d5875618448a679e92a178866ce22ec6b08b
doguto <doguto110@gmail.com>
2024-10-22 13:59:14 +0900

    [Feat] prompt

--- microservices/text_transition/test/test.py
+++ microservices/text_transition/test/test.py
@@ -5,6 +5,6 @@ from .. import prompt
 prompt = prompt.Prompt()
 text_transer = text_transer.Text_Transer_GPT()
 
-prompt_text = prompt.get_prompt("BtoBではなくBtoCの方向で考えよう", 2)
+prompt_text = prompt.get_prompt("直線の通過領域問題を考えるときはファクシミリ論法も有効であるが、偏微分から包絡線を考えた方が楽であることの方が多い。", 3)
 print(text_transer.process_text_with_chatgpt(prompt_text))
 
